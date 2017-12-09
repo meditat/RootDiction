5638573d03db4aafe2c11553103345d8c956de6a
shivam Singh <shivam.15bme1094@abes.ac.in>
2017-12-09 22:47:15 +0530

    Privacy Issue

--- src/com/meditat/GetSomeThing.java
+++ src/com/meditat/GetSomeThing.java
@@ -2,7 +2,7 @@ package com.meditat;
 
 public class GetSomeThing {
 
-    final String app_id = "48f66cfc";
-    final String app_key = "5168a28b3399b7b671bc4567156fe8eb";
+    final String app_id = "         ";
+    final String app_key = "                      ";
 
 }
